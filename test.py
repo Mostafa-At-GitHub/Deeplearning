eb2d60afc3a2a939df74ebdf9420788abaf5799e
koheikawasaki <k.kwsk.lki@gmail.com>
2016-09-25 00:23:11 +0900

    Update

--- test.py
+++ test.py
@@ -51,7 +51,8 @@ if False:
     print df.sum()
     print df.sum(axis=1)
     print df.values.sum()
-
+<<<<<<< HEAD
+=======
     
 def mean_riders_for_max_station(ridership):
     '''
@@ -137,3 +138,227 @@ def get_hourly_entries_and_exits(entries_and_exits):
     '''
     return None
 
+import pandas as pd
+
+# Change False to True for this block of code to see what it does
+
+# DataFrame applymap()
+if 0:
+    df = pd.DataFrame({
+        'a': [1, 2, 3],
+        'b': [10, 20, 30],
+        'c': [5, 10, 15]
+    })
+    
+    def add_one(x):
+        return x + 1
+        
+    print df.applymap(add_one)
+    
+grades_df = pd.DataFrame(
+    data={'exam1': [43, 81, 78, 75, 89, 70, 91, 65, 98, 87],
+          'exam2': [24, 63, 56, 56, 67, 51, 79, 46, 72, 60]},
+    index=['Andre', 'Barry', 'Chris', 'Dan', 'Emilio', 
+           'Fred', 'Greta', 'Humbert', 'Ivan', 'James']
+)
+def grading(x):
+    if x <= 100 and x >=90:
+        return "A"
+    elif x < 90 and x >= 80:
+        return "B"
+    elif x < 80 and x >= 70:
+        return "C"
+    elif x < 70 and x >= 60:
+        return "D"
+    elif x < 60 and x >= 0:
+        return "F"
+    else:
+        return "None"
+def convert_grades(grades):
+    '''
+    Fill in this function to convert the given DataFrame of numerical
+    grades to letter grades. Return a new DataFrame with the converted
+    grade.
+    
+    The conversion rule is:
+        90-100 -> A
+        80-89  -> B
+        70-79  -> C
+        60-69  -> D
+        0-59   -> F
+    '''
+    return grades.applymap(grading)
+    
+Note: In order to get the proper computations, we should actually be setting the value of the "ddof" parameter to 0 
+in the .std() function.
+
+Note that the type of standard deviation calculated by default is different between numpy's .std() and pandas' .std() functions.
+By default, numpy calculates a population standard deviation, with "ddof = 0". On the other hand, 
+pandas calculates a sample standard deviation, with "ddof = 1". If we know all of the scores, 
+then we have a population - so to standardize using pandas, we need to set "ddof = 0".
+import pandas as pd
+
+grades_df = pd.DataFrame(
+    data={'exam1': [43, 81, 78, 75, 89, 70, 91, 65, 98, 87],
+          'exam2': [24, 63, 56, 56, 67, 51, 79, 46, 72, 60]},
+    index=['Andre', 'Barry', 'Chris', 'Dan', 'Emilio', 
+           'Fred', 'Greta', 'Humbert', 'Ivan', 'James']
+)
+
+# Change False to True for this block of code to see what it does
+
+# DataFrame apply()
+if 1:
+    def convert_grades_curve(exam_grades):
+        # Pandas has a bult-in function that will perform this calculation
+        # This will give the bottom 0% to 10% of students the grade 'F',
+        # 10% to 20% the grade 'D', and so on. You can read more about
+        # the qcut() function here:
+        # http://pandas.pydata.org/pandas-docs/stable/generated/pandas.qcut.html
+        return pd.qcut(exam_grades,
+                       [0, 0.1, 0.2, 0.5, 0.8, 1],
+                       labels=['F', 'D', 'C', 'B', 'A'])
+        
+    # qcut() operates on a list, array, or Series. This is the
+    # result of running the function on a single column of the
+    # DataFrame.
+    print convert_grades_curve(grades_df['exam1'])
+    
+    # qcut() does not work on DataFrames, but we can use apply()
+    # to call the function on each column separately
+    print grades_df.apply(convert_grades_curve)
+    
+def standardize(df):
+    '''
+    Fill in this function to standardize each column of the given
+    DataFrame. To standardize a variable, convert each value to the
+    number of standard deviations it is above or below the mean.
+    '''
+    return (df - df.mean()) / df.std(ddof = 0)
+# Adding using +
+if 0:
+    s = pd.Series([1, 2, 3, 4])
+    df = pd.DataFrame({
+        0: [10, 20, 30, 40],
+        1: [50, 60, 70, 80],
+        2: [90, 100, 110, 120],
+        3: [130, 140, 150, 160]
+    })
+    
+    print df
+    print '' # Create a blank line between outputs
+    print df + s
+    
+# Adding with axis='index'
+if 0:
+    s = pd.Series([1, 2, 3, 4])
+    df = pd.DataFrame({
+        0: [10, 20, 30, 40],
+        1: [50, 60, 70, 80],
+        2: [90, 100, 110, 120],
+        3: [130, 140, 150, 160]
+    })
+    
+    print df
+    print '' # Create a blank line between outputs
+    print df.add(s, axis='index')
+    # The functions sub(), mul(), and div() work similarly to add()
+    
+# Adding with axis='columns'
+if 0:
+    s = pd.Series([1, 2, 3, 4])
+    df = pd.DataFrame({
+        0: [10, 20, 30, 40],
+        1: [50, 60, 70, 80],
+        2: [90, 100, 110, 120],
+        3: [130, 140, 150, 160]
+    })
+    
+    print df
+    print '' # Create a blank line between outputs
+    print df.add(s, axis='columns')
+    # The functions sub(), mul(), and div() work similarly to add()
+    
+grades_df = pd.DataFrame(
+    data={'exam1': [43, 81, 78, 75, 89, 70, 91, 65, 98, 87],
+          'exam2': [24, 63, 56, 56, 67, 51, 79, 46, 72, 60]},
+    index=['Andre', 'Barry', 'Chris', 'Dan', 'Emilio', 
+           'Fred', 'Greta', 'Humbert', 'Ivan', 'James']
+)
+# print grades_df
+def standardize(df):
+    '''
+    Fill in this function to standardize each column of the given
+    DataFrame. To standardize a variable, convert each value to the
+    number of standard deviations it is above or below the mean.
+    
+    This time, try to use vectorized operations instead of apply().
+    You should get the same results as you did before.
+    '''
+    return (df - df.mean()) / df.std(ddof = 0)
+
+def standardize_rows(df):
+    '''
+    Optional: Fill in this function to standardize each row of the given
+    DataFrame. Again, try not to use apply().
+    
+    This one is more challenging than standardizing each column!
+    DataFrame.mean(axis=None, skipna=None, level=None, numeric_only=None, **kwargs)
+    '''
+    # print df
+    mean = df.mean(axis='columns')
+    # print mean
+    diff = df.sub(mean, axis='index')
+    # print diff
+    std = df.std(axis='columns', ddof= 0)
+    print std
+    return diff.div(std, axis='index')
+standardize_rows(grades_df)
+values = np.array([1, 3, 2, 4, 1, 6, 4])
+example_df = pd.DataFrame({
+    'value': values,
+    'even': values % 2 == 0,
+    'above_three': values > 3 
+}, index=['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+
+# Change False to True for each block of code to see what it does
+
+# Examine DataFrame
+if 0:
+    print example_df
+    
+# Examine groups
+if 1:
+    grouped_data = example_df.groupby('even')
+    # The groups attribute is a dictionary mapping keys to lists of row indexes
+    print grouped_data.groups
+    
+# Group by multiple columns
+if 1:
+    grouped_data = example_df.groupby(['even', 'above_three'])
+    print grouped_data.groups
+    
+# Get sum of each group
+if 1:
+    grouped_data = example_df.groupby('even')
+    print grouped_data.sum()
+    
+# Limit columns in result
+if 1:
+    grouped_data = example_df.groupby('even')
+    
+    # You can take one or more columns from the result DataFrame
+    print grouped_data.sum()['value']
+    
+    print '\n' # Blank line to separate results
+    
+    # You can also take a subset of columns from the grouped data before 
+    # collapsing to a DataFrame. In this case, the result is the same.
+    print grouped_data['value'].sum()
+    
+filename = './nyc_subway_weather.csv'
+subway_df = pd.read_csv(filename)
+ridership_by_day = subway_df.groupby('day_week').mean()[]
+%pylab inline
+import seaborn as sb
+ridership_by_day.plot()
