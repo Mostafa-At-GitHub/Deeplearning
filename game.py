e7629823d0d18e2a7a69a02d29c4ded0bdcb357b
koheikawasaki <k.kwsk.lki@gmail.com>
2016-09-25 20:12:42 +0900

    Create game.py

---
+++ game.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+width, height, my_id = [int(i) for i in raw_input().split()]
+
+# game loop
+while True:
+    for i in xrange(height):
+        row = raw_input()
+    entities = int(raw_input())
+    for i in xrange(entities):
+        entity_type, owner, x, y, param_1, param_2 = [int(j) for j in raw_input().split()]
+        print entiry
+
+    # Write an action using print
+    # To debug: print >> sys.stderr, "Debug messages..."
+
+    print "BOMB 6 5"
