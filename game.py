f8bb32ecf46d0c41af67dec0eb26bf20320e1bca
voyageanp <nwcntyrcrm@gmail.com>
2016-09-25 20:16:12 +0900

    add debag messages

--- game.py
+++ game.py
@@ -10,10 +10,16 @@ width, height, my_id = [int(i) for i in raw_input().split()]
 while True:
     for i in xrange(height):
         row = raw_input()
+        print row
     entities = int(raw_input())
     for i in xrange(entities):
         entity_type, owner, x, y, param_1, param_2 = [int(j) for j in raw_input().split()]
-        print entiry
+        print entity_type
+        print owner
+        print x
+        print y
+        print param_1
+        print param_2
 
     # Write an action using print
     # To debug: print >> sys.stderr, "Debug messages..."
